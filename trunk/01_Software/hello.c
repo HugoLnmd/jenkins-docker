3f8967f99a8714b807d321f372e953f7e25c2784
HugoLnmd <99959213+HugoLnmd@users.noreply.github.com>
2022-03-09 10:43:27 +0100

    Add files via upload

---
+++ trunk/01_Software/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	
+	int i;
+	
+	printf("Hello World ! Executed hello.c");
+   
+	return 0;
+}
---
+++ trunk/01_Software/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+
+int main() {
+
+	int i;
+	
+	printf("Hello World ! Executed test.c");
+
+	return 0;
+}
